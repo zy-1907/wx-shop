99e485d20ab2383977d6d179661dbeb8c35cd416
zy-1907 <8826928+zhang1907@user.noreply.gitee.com>
2021-05-06 17:19:41 +0800

    购物车页面

--- pages/cart/cart.js
+++ pages/cart/cart.js
@@ -1,66 +1,30 @@
-// pages/cart/cart.js
+/* 
+1 获取用户的收货地址
+  1 绑定点击事件
+  2 调用小程序内置 api 获取用户地址 wx.choosesAddress
+
+2 页面加载完毕
+  0 onLoad->页面第一次加载时触发，从跳转页面返回时不能触发，可以传递参数 onShow->页面显示或从后台跳回小程序时显示此页面时触发，从跳转页面返回时触发，不能传递参数
+  1 获取本地存储中的地址数据
+  2 把数据 设置给data中的一个变量
+*/
 Page({
-
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
+  data:{
+    address:{}
   },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
+  onShow(){
+    // 1 获取缓存中的收货地址信息
+    const address=wx.getStorageSync('address');
+    this.setData({
+      address
+    })
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
+  // 点击 收货地址
+  handleChooseAddress(){
+    wx.chooseAddress({
+      success: (result) => {
+         wx.setStorageSync('address', result);
+      } 
+    });
   }
 })
--- pages/goods_detail/goods_detail.js
+++ pages/goods_detail/goods_detail.js
@@ -1,3 +1,16 @@
+/* 
+1 发送请求获取数据
+2 点击轮播图 预览大图
+  1 给轮播图绑定点击事件
+  2 调用小程序的api previewImage
+3 点击 加入购物车
+  1 先绑定点击事件
+  2 获取缓存中的购物车数据 数组格式
+  3 先判断 当前商品是否已经存在于 购物车
+  4 已经存在 修改商品数据 执行购物车数量++ 重新把购物车数组 填充回缓存中
+  5 不存在于购物车的数组中 直接给购物车数组添加一个新元素 新元素带上 购买数量属性 num 重新把购物车数组 填充回缓存中
+  6 弹出提示
+  */
 import {request} from "../../request/index.js";
 Page({
 
@@ -7,7 +20,8 @@ Page({
   data: {
     goodsObj:{}
   },
-
+  //商品对象
+  GoodsInfo:{},
   /**
    * 生命周期函数--监听页面加载
    */
@@ -19,10 +33,58 @@ Page({
   //获取商品详情数据
   async getGoodsDetail(goods_id){
     const res = await request({url:"/goods/detail",data:{goods_id}})
-
+    const goodsObj=res.data.message
+    this.GoodsInfo=goodsObj;
     this.setData({
-      goodsObj:res.data.message
+      goodsObj:{
+        goods_name:goodsObj.goods_name,
+        goods_price:goodsObj.goods_price,
+        //iphone部分手机 不识别 webp图片格式
+        //最好找后端改jpg
+        //临时自己改 1.webp=>1.jpg
+        goods_introduce:goodsObj.goods_introduce.replace(/\.webp/g,),
+        pics:goodsObj.pics
+      }
     })
+  },
+  //点击轮播图放大预览
+  handlePrevewImage(e){
+    //先构造要预览的图片数组
+    const urls = this.GoodsInfo.pics.map(v=>v.pics_mid);
+    //接收传递过来的图片url
+    const current = e.currentTarget.dataset.url;
+    wx.previewImage({
+      current,
+      urls
+    })
+  },
+  //点击加入购物车
+  handleCartAdd(){
+   // 1 获取缓存中的购物车 数组
+   let cart = wx.getStorageSync('cart') || [];
+   // 2 判断 商品对象是否存在于购物车数组中
+   let index = cart.findIndex(v=>v.goods_id===this.GoodsInfo.goods_id);
+   if(index===-1){
+    //不存在
+    this.GoodsInfo.num=1;
+    cart.push(this.GoodsInfo);
+   }else{
+    //已存在购物车数据 执行 num++
+    cart[index].num++;
+   }
+   //把购物车重新添加回缓存中
+   wx.setStorageSync('cart', cart);
+   //提示
+   wx-wx.showToast({
+     title: '加入成功',
+     icon: 'success',
+     // true 防止用户手抖 疯狂点击按钮
+     mask: true,
+     success: (res) => {},
+     fail: (res) => {},
+     complete: (res) => {},
+   })
   }
+
   
 })
